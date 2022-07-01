3b304d0bde92e5a871d9d5daf652f5b05c782dd6
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-07-01 10:22:17 +0500

    test build

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import "./app.scss";
 import App from './App.svelte'
 
 const app = new App({
-  target: document.getElementById('app')
+  // target: document.getElementById('app')
+  target: document.querySelector('section div')
 })
 
 export default app
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+// import Roll from './rollup.config.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+      // input:'src/main.js',
+      //  output:{
+      //    dir:'public/build',
+       //   format:'iife',
+
+      //   name:'app',
+      //   // file:'public/build/bundle.js'
+
+      // }
+    }
+  },
 
   css: {
     preprocessorOptions: {
